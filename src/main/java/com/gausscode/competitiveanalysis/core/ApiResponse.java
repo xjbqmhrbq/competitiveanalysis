1b1f4ac5d622ef63132c53de3d8110939f09558e
xiao.lu <xiao.lu@gausscode.com>
2024-01-03 10:16:52 +0800

    初始文件上传

---
+++ src/main/java/com/gausscode/competitiveanalysis/core/ApiResponse.java
@@ -0,0 +1,73 @@
+package com.gausscode.competitiveanalysis.core;
+
+import java.io.Serializable;
+import java.util.StringJoiner;
+
+public class ApiResponse<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public ApiResponse(Integer code,String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public ApiResponse(Integer code,String message,T data){
+        this(code,message);
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(){
+        return new ApiResponse<>(ResponseCode.SUCCESS.getCode(),ResponseCode.SUCCESS.getMessage());
+    }
+
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<>(ResponseCode.SUCCESS.getCode(),ResponseCode.SUCCESS.getMessage(),data);
+    }
+
+    public static <T> ApiResponse<T> fail(){
+        return new ApiResponse<>(ResponseCode.FAILURE.getCode(),ResponseCode.FAILURE.getMessage());
+    }
+
+    public static <T> ApiResponse<T> fail(Integer code,String message){
+        return new ApiResponse<>(code,message);
+    }
+
+    public static <T> ApiResponse<T> error(){
+        return new ApiResponse<>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getMessage());
+    }
+
+    public void setCode(Integer code){
+        this.code = code;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public void setData(T data){
+        this.data = data;
+    }
+    public Integer getCode(){
+        return this.code;
+    }
+    public String getMessage(){
+        return this.message;
+    }
+    public T getData(){
+        return this.data;
+    }
+
+    @Override
+    public String toString() {
+        return (new StringJoiner(", ", ApiResponse.class.getSimpleName() + "[", "]"))
+                .add("code=" + this.code)
+                .add("message='" + this.message + "'")
+                .add("data=" + this.data)
+                .toString();
+    }
+}
---
+++ src/test/java/com/gausscode/competitiveanalysis/CompetitiveanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gausscode.competitiveanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitiveanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
